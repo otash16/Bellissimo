97160c4215d5bf4bdd17c8a8c64839ea01f119ad
otash16 <otajonqutbiddinov1@gmail.com>
2025-05-25 13:44:56 +0500

    feat: member service model integration

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from "express";
 import {T} from '../libs/types/common'
+import MemberService from "../models/member.service";
 
 const restaurantController: T = {}; 
 
@@ -23,7 +24,7 @@ restaurantController.getSignup = (req: Request, res: Response) => {
     try{
         res.send("Signup Page");
     } catch(err) {
-        console.log('ERROR, Signu[]: ',err);
+        console.log('ERROR, Signup: ',err);
     }
 }
 
