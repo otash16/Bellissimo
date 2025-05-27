3b5613875fac7cadff47af51e33e1276b1521fd8
otash16 <otajonqutbiddinov1@gmail.com>
2025-05-27 15:14:57 +0500

    feat: create project standarts

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -6,6 +6,7 @@ const restaurantController: T = {};
 
 restaurantController.goHome = (req: Request, res: Response) => {
     try{
+        console.log("goHome");
         res.send("Home Page");
     } catch(err) {
         console.log('ERROR, goHome: ',err);
@@ -14,6 +15,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 
 restaurantController.getLogin = (req: Request, res: Response) => {
     try{
+        console.log("getlogin");
         res.send("Login Page");
     } catch(err) {
         console.log('ERROR, Login: ',err);
@@ -22,6 +24,7 @@ restaurantController.getLogin = (req: Request, res: Response) => {
 
 restaurantController.getSignup = (req: Request, res: Response) => {
     try{
+        console.log("getSignup");
         res.send("Signup Page");
     } catch(err) {
         console.log('ERROR, Signup: ',err);
