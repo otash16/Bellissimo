c0bbb76ace091825160e20fd8953e99fe92ed8d7
otash16 <otajonqutbiddinov1@gmail.com>
2025-07-08 04:17:39 +0500

    feat: locals variable integration

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from "express";
+import {NextFunction, Request, Response} from "express";
 import {T} from '../libs/types/common'
 import MemberService from "../models/member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
@@ -113,5 +113,17 @@ restaurantController.checkAuthSession = async (req: AdminRequest, res: Response)
         console.log("Error, checkAuthSession:", err);
         res.send(err);
     }
+};
+
+restaurantController.verifyRestaurant = (req: AdminRequest, res: Response, next: NextFunction) => {
+        if(req.session?.member?.memberType === MemberType.RESTAURANT) {
+            req.member = req.session.member;
+            next();
+        } else {
+            const message = Message.NOT_AUTHENTICATED;
+            res.send(
+                `<script> alert("${message}"); window.location.replace('/admin/login'); </script>`
+            );
+        }
 };
 export default restaurantController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -19,9 +19,22 @@ routerAdmin.get('/logout', restaurantController.logout);
 routerAdmin.get('/check-me', restaurantController.checkAuthSession);
 
 /**PRODUCTS */
-routerAdmin.get('/product/all', productController.getAllProducts);
-routerAdmin.post('/product/create', productController.createNewProduct);
-routerAdmin.post('/product/:id', productController.updateChosenProduct);
+routerAdmin.get
+('/product/all', 
+ restaurantController.verifyRestaurant,
+ productController.getAllProducts
+);
+
+routerAdmin.post(
+'/product/create', 
+    restaurantController.verifyRestaurant, 
+    productController.createNewProduct);
+
+routerAdmin.post(
+'/product/:id', 
+ restaurantController.verifyRestaurant,
+ productController.updateChosenProduct
+);
 
 /** USERS */
 
